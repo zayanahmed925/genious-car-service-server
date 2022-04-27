32d68923da6a8e05a18ce3661381b0f0ac3cc0ef
zayanahmed925 <rjraju925@gmail.com>
2022-04-28 01:55:49 +0600

    create order and get order data automatic in form,handle different user using query and create jwt simple ceate

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -19,6 +20,16 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniousCar').collection('service');
+
+        const orderCollection = client.db('geniousCar').collection('order');
+        //Access Token
+        app.post('/login', (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
         //For All data load
         app.get('/service', async (req, res) => {
             const query = {};
@@ -45,6 +56,21 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+        ////Order collection api////
+
+        //User find
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
             res.send(result);
         })
     }
